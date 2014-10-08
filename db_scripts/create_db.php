7ae0fd03a3cff36987150d9669909b2957eed1c5
Scott Russell <scottedwardrussell@gmail.com>
2014-10-08 17:28:54 -0400

    Refactored create_db for PDO db interaction

--- db_scripts/create_db.php
+++ db_scripts/create_db.php
@@ -1,29 +1,26 @@
 <?php
 
-	//create_db.php - Script to create new MySQL database
+    //create_db.php - Script to create new MySQL database
 
-	include "cred_ext.php";
+    include "cred_ext.php";
 
-	//Create connection
-	$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
-	// Check connection
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-
+   try {
+   	//Create connection
+	$dbh = new PDO('mysql:host=localhost', DB_USERNAME, DB_PASSWORD);
 
 	// Create database
-	$sql = "CREATE DATABASE test_db";
-	if (mysqli_query($con, $sql)) {
-		echo "Database test_db created successfully.\n";
-	}
-	else {
-	 	echo "Error creating database: " . mysqli_error($con);
-	}
+	$stmt = $dbh->prepare('CREATE DATABASE test_db');
+        $stmt->execute();
 
+	echo "Database test_db created successfully.\n";
 
-
-	// Close connection
-	mysqli_close($con);
+        $stmt->closeCursor();
+        $stmt = null;
+        $dbh = null;
+    }
+    catch (PDOException $e) {
+        print "Error: " . $e->getMessage() . "<br/>";
+        die();
+    }
 
 ?>
