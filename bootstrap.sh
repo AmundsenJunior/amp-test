4a3843fea7f573ba4b0683391c31dd60c7b254a3
Scott Russell <scottedwardrussell@gmail.com>
2014-10-09 16:35:58 -0400

    Added Vagrantfile; bootstrap is functional until lamp-server command

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y upgrade
+apt-get install -y vim git
+#apt-get install -y lamp-server^
+#a2enmod php5
+#php5enmod pdo
+#php5enmod pdo_mysql
+#service apache2 restart
+#apt-get install -y phpmyadmin
+#rm -rf /var/www
+#ln -fs /vagrant /var/www
