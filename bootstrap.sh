4a7f645acb605d3005486c5d541803a705eee56a
Scott Russell <scottedwardrussell@gmail.com>
2014-10-13 16:30:18 -0400

    Added phpmyadmin install commands to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,14 @@ echo "mysql-server mysql-server/root_password password $DBPASSWORD" | debconf-se
 echo "mysql-server mysql-server/root_password_again password $DBPASSWORD" | debconf-set-selections
 apt-get -y install lamp-server^
 
+echo -e "\n--- Install phpMyAdmin ---\n"
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" |debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWORD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWORD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWORD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+apt-get -y install phpmyadmin
+
 #a2enmod php5
 #php5enmod pdo
 #php5enmod pdo_mysql
