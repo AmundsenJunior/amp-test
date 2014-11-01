d10fa991d6bd916f0a3b33c9ff8f8e8e75a3be2a
Scott Russell <scottedwardrussell@gmail.com>
2014-10-31 22:29:25 -0400

    Removed git, vim from install; enabled apache commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,6 @@ echo -e "\n--- Updating/upgrading packages list ---\n"
 apt-get update
 apt-get -y upgrade
 
-echo -e "\n--- Installing vim and git ---\n"
-apt-get -y install vim git
-
 echo -e "\n--- Install LAMP and configure Server ---\n"
 echo "mysql-server mysql-server/root_password password $DBPASSWORD" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DBPASSWORD" | debconf-set-selections
@@ -26,10 +23,12 @@ echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWORD" | debconf-set-s
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
 apt-get -y install phpmyadmin
 
-#a2enmod php5
-#php5enmod pdo
-#php5enmod pdo_mysql
-#service apache2 restart
-#apt-get install -y phpmyadmin
-#rm -rf /var/www
-#ln -fs /vagrant /var/www
+echo -e "\n--- Enable Apache modules and link /var/www/ to project ---\n"
+a2enmod php5
+php5enmod pdo
+php5enmod pdo_mysql
+service apache2 restart
+apt-get install -y phpmyadmin
+rm -rf /var/www
+ln -fs /vagrant /var/www
+
